1ded42047506ee06da45342354981d2c522c1658
lan496 <kshinohara0508@gmail.com>
2024-02-03 16:39:49 +0900

    Check error message

--- test/functional/python/test_cell.py
+++ test/functional/python/test_cell.py
@@ -4,5 +4,5 @@ from spglib.spglib import _expand_cell
 
 def test_expand_cell():
     with pytest.raises(TypeError) as e:
-        _expand_cell(None)
-    assert e.value
+        _expand_cell(([], [], [], [], []))
+    assert str(e.value) == "cell has to be a tuple of 3 or 4 elements."
